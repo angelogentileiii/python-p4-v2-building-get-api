f3bbc7c0983fc1d4801d72367206870dbdbbb30b
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-12-11 10:00:53 -0500

    completed code along

--- server/app.py
+++ server/app.py
@@ -21,6 +21,35 @@ def index():
 
 # start building your API here
 
+@app.route('/games')
+def games():
+
+    games = Game.query.all()
+    games_data = [g.to_dict() for g in games]
+
+    response = make_response(jsonify(games_data), 200)
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id: int):
+    game = Game.query.get(id)
+
+    if not game:
+        return make_response(jsonify({'error': f'The game does not currently exist.'}), 404)
+    
+    game_dict = game.to_dict()
+
+    response = make_response(jsonify(game_dict), 200)
+    return response
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id: int):
+    game = Game.query.get(id)
+
+    users = [r.user.to_dict() for user in game.users]
+
+    response = make_response(jsonify(users), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
